c8d004c1d02fdccb283ead323d1b1064e318b737
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-27 02:07:31 +0400

    Update L19.py
    recap

--- L19.py
+++ L19.py
@@ -1,1 +1,13 @@
+user_promt = "Enter a todo: "
 
+todo1 = input(user_promt)
+todo2 = input(user_promt)
+todo3 = input(user_promt)
+# variables and values in the list
+todos = [todo1, todo2, todo3, "Hi"]
+
+print (todos) 
+# type of the written part
+print(type(todo) 
+
+        ]
