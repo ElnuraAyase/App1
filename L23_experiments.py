8d713650bf8b5153faf82d67b05e40d32c3c00b8
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-29 20:40:32 -1100

    Create L23_experiments.py

---
+++ L23_experiments.py
@@ -0,0 +1,18 @@
+# The correct way 
+user_promt = "enter the task: "
+
+todos [] #empty listoutside the loop 
+
+while True:
+  todo = input(user_promt)  
+  todos.append(todo) #add all todo to a list with todos
+  
+# if place user promt inputinside loop 
+
+
+todos [] #empty listoutside the loop 
+
+while True:
+  user_promt = "enter the task: " # gives the same outputbut the code is heavier
+  todo = input(user_promt)  
+  todos.append(todo) #add all todo to a list with todos
