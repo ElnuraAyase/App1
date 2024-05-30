5b3e6179230dc5c1ac4311f0da6794ad1feac5c9
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-29 19:22:50 -1100

    Update L20.py
    boolean true for loop

--- L20.py
+++ L20.py
@@ -6,3 +6,11 @@ while 2>1:
   todo = input(user_promt) #condition goes here + addd indentation(space after : ) 
 print(todo)
 print("Next: ")
+
+
+user_promt = "enter the task: "
+
+while true: # true is a boolean, if it's true it will repeat the loop 
+  todo = input(user_promt) #condition goes here + addd indentation(space after : ) 
+print(todo)
+print("Next: ")
