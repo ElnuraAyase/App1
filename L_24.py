9eec1bf8fe3976750d5f6e40a11a7ffb29c2b0b7
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-05-31 23:58:09 -1200

    Update L_24.py

--- L_24.py
+++ L_24.py
@@ -4,3 +4,8 @@ password = input("Enter your password here: ")
 while password != "pass123":
   password = input("Enter the password here: ")
 print("password was correct")
+
+
+#another loop
+x=1
+while x<=6
