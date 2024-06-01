a0a1500b270d7d9ab167559c5e479a64a69bfe00
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-01 00:36:56 -1200

    Update L25Exercises.py

--- L25Exercises.py
+++ L25Exercises.py
@@ -1,1 +1,26 @@
+#just method usage,simple code
+name = input("Enter your name: ")
+print(name.capitalize())
 
+
+#while loop 
+
+name = input("Enter your name: ")
+while True:
+    print(name.capitalize())
+  
+# stop the loop when it reaches 20 names
+name = input("Enter your name: ")
+count = 0 
+while count < 20:
+  print(name.capitalize())
+  count += 1
+
+print("it was printed 20 times.") # messages that will come after the loop
+
+#loop the input from user for new names
+
+name = input("Enter your name: ")
+while True:
+  print(name.capitalize())
+  input 
