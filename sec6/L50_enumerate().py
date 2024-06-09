88f733921496eeb47fb56dc799fea72c54420fce
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 01:16:09 +0400

    Update L50_enumerate().py

--- sec6/L50_enumerate().py
+++ sec6/L50_enumerate().py
@@ -1,3 +1,51 @@
 
+
+# add edit
+todos = []
+
+while True:
+    user_action = input("Type add, edit,  show or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo= input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+# add edit
+        case'edit':
+# list-indexing 
+            number= input("Enter the number of a todo to edit: ")
+            existing_todos = todo [number] 
+            print(existing_todos)     
+        case 'exit':
+            break
+print("Bye!")
+
+# replacing todo for the edit choice
+todos = []
+
+while True:
+    user_action = input("Type add, edit,  show or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo= input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case'edit':
+            number= int(input("Enter the nuber of a todo to edit: "))
+            number = number -1
+            new_todo = input(enter new todo: ")
+            todos[number] = new_todo
+        case 'exit':
+            break
         case _: # 1. for any other input discluding the offered ones
              print("The command is unknown.")
+print("Bye!")
+
