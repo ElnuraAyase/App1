8cfe6f5079ae6a13e51fa13a2e9c36ad26059561
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 17:04:45 +0400

    Create simplifiedCode52.py

---
+++ sec6/simplifiedCode52.py
@@ -0,0 +1,68 @@
+
+# replacing todo for the edit choice
+todos = []
+
+while True:
+    user_action = input("Type add, edit,  show, remove or exit: ").lower().strip()
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo= input("Enter a todo or press 'x' to go back: ")
+            if todo.lower() != 'x':
+                todos.append(todo)
+        case 'show':
+            if todos:
+                for index, item in enumerate(todos):   # enumerate()
+                    row = f"{index + 1}-{item}"            #f string
+                    print(row)
+            else:
+                print("The list is empty, try add a todo ")
+                todo = input("enter a todo, (or press 'x' to go back): ").strip()
+                if todo.lower() !='x':
+                    todos.append(todo)
+        case'edit':
+            if todos:
+                while True:
+                    number = (input("Enter the number of a todo to edit(or press 'x' to go back): "))
+                    if number.lower() == 'x':
+                        break
+                    try:
+                        number = int(number) -1
+                        if 0 <= number < len(todos):
+                            new_todo = input("Enter new todo, or 'x' to go back: ").strip()
+                            todos[number] = new_todo
+                            break
+                        else:
+                            print("Invalid number, enter a lower number ")
+                    except ValueError:
+                        retry = input("Enter a valid number or enter 'x',to go back: ").lower().strip()
+            else:
+                print("list is empty,there is nothing to edit, try to add a todo")
+                todo = input("enter a todo, (or press 'x' to go back): ").strip()
+                if todo.lower() !='x':
+                    todos.append(todo)
+        case 'remove':                                             # remove - delete statment
+            if todos:
+                while True:
+                    try:
+                        number = (input("Enter number of todo to delete: "))  # for number, as pop removes numbers
+                        if number.lower() == 'x':
+                            break
+                        number = int(number)-1
+                        if 0 <= number < len(todos):
+                            new_todo = input("Enter new todo, or 'x' to go back: ").strip()
+                            todos[number]= new_todo
+                            break
+                        else:
+                            print("Invalid number, enter a lower number ")
+                    except ValueError:
+                        retry = input("Enter a valid number or enter 'x',to go back: ")
+
+            todos.pop(number)
+        case 'exit':
+            break
+        case _: # 1. for any other input discluding the offered ones
+             print("The command is unknown.")
+print("Bye!")
+
