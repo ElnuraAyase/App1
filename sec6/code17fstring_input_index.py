215de5b989aa4c511c259f875ebe85c802e0fc59
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 20:54:12 +0400

    Create code17fstring_input_index.py

---
+++ sec6/code17fstring_input_index.py
@@ -0,0 +1,18 @@
+ips = ['100.122.133.105', '100.122.133.111']
+usr = int(input("Enter the number of IPs: "))
+choise = f"You chose {ips[usr]}"
+print(choise)
+
+'''
+We have a list of two IPs in the code area.
+
+Extend the code so your program:
+
+1. Prompts the user to input an index (e.g., 0 or 1).
+
+2. Depending on the user input, the program should return either You chose 100.122.133.111 or You chose 100.122.133.111
+
+Note: In order for the system to mark your exercise as correct, 
+your code should return the exact output
+(i.e., upper case Y in You chose and no spaces or other characters after the IP.
+'''
