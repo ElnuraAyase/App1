4fc0b891b1cc03730abdfb6a5fcc80eb04ce3546
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 20:57:50 +0400

    Update code18_differentItemsInList.py

--- sec6/code18_differentItemsInList.py
+++ sec6/code18_differentItemsInList.py
@@ -1,5 +1,14 @@
+#ex 1
+# Assign a list to the variable temperatures. The list should contain three items, a float, an integer, and a string.
 temperatures = [4.4, 4, 'sjd']
 print(temperatures)
 
 
-# Assign a list to the variable temperatures. The list should contain three items, a float, an integer, and a string.
+
+# ex 2
+'''
+Assing a list to the rainfallvariable. The list should contain four items, a float, an integer, a string, and a list*. 
+
+*It's totally fine for a list to contain another list as an item
+'''
+rainfall = [4.4, 4, 'f', [4.3, 3, 'e']]
