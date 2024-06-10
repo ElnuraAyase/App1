e9d76ed1775a8fc88f22b8240273693dc725c65d
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 19:37:42 +0400

    Update L54experiment.py
    2 examples of experiment with index printing

--- sec6/L54experiment.py
+++ sec6/L54experiment.py
@@ -1,1 +1,37 @@
 
+#example 1
+todos = []
+while True:
+    user_action = input("Type add, edit,  show or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo= input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for index, item in enumerate(todos):                   
+                row = f"{index+1}-{item}"                          
+                print(row)
+            print ("Hello!", index, item)            # output will print hello and last index and  item           
+# output 
+'''
+1-work
+2-write
+3-eat
+Hello! 3 eat 
+'''
+
+# example 2
+myList= ['a','s', 'd']
+for index,item in enumerate(myList):
+    row = f"{index+1}-{item}"
+    print(row)
+print('Hello!', index, item)
+# output
+'''
+1-a
+2-s
+3-d
+Hello 2 d
+'''
