729b5ff7999295ee23cce08117a762257c9ae2a3
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-10 11:53:50 +0400

    Update my_adds.py
    1. added go back function to show and edit , at several stages
    2. moved else from while,so it functions now
    3. used strip() and lower()
    4. used retry
    5. used try / exept

--- sec6/my_adds.py
+++ sec6/my_adds.py
@@ -8,16 +8,18 @@ while True:
 
     match user_action:
         case 'add':
+            # added x to go back 
             todo= input("Enter a todo: ")
-            todos.append(todo)
+            if todos.lower() != 'x':
+                todos.append(todo)
         case 'show':
             if todos:
                 for index, item in enumerate(todos) :
                     print(index +1, '-', item)# enumerate()
             else:
                 print("The list is empty, try add a todo ")
-                todo = input("enter a todo, (or press 'x' to go back): ").strip()
-                if todo.lower() !='x':
+                todo = input("Enter a todo, (or press 'x' to go back): ").strip()
+                if todo.lower() !='x':                                               # go back if don't want to enter todo
                     todos.append(todo)
         case'edit':
             if todos:
@@ -25,22 +27,22 @@ while True:
                     try:
                         number = int(input("Enter the number of a todo to edit: "))
                         number = number -1
-                        if 0 <= number < len(todos):                             
-                            new_todo = input("Enter new todo: ")
+                        if 0 <= number < len(todos):     
+                            new_todo = input("Enter new todo or 'x' to go back: ").strip()    #  to go back, in case you don't want to change the to do
+                            if todo.lower() != 'x':
                             todos[number] = new_todo
-                            break
-                          # all new lines 
+                            break                                                              # back to new lines
                         else:
                             print("Invalid number, enter a lower number ")               
                     except ValueError:
                         retry = input("Enter a valid number or enter 'x',to go back: ").lower().strip()
                         if retry == 'x':
                             break
-                else:
-                    print("list is empty,there is nothing to edit, try to add a todo")
-                    todo = input("enter a todo, (or press 'x' to go back): ").strip()
-                    if todo.lower() !='x':
-                        todos.append(todo)
+            else:                                                                                 # moved out of whileloop so it can work 
+                print("list is empty,there is nothing to edit, try to add a todo")
+                todo = input("enter a todo, (or press 'x' to go back): ").strip()
+                if todo.lower() !='x':
+                    todos.append(todo)
 
         case 'exit':
             break
