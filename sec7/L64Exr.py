fe759cdd3dc4c6e650b3fa6ec22b2277fe3c7b1c
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-22 23:46:24 -1200

    Update L64Exr.py

--- sec7/L64Exr.py
+++ sec7/L64Exr.py
@@ -1,1 +1,8 @@
+'''
+Take a look at the essay.txt file tab. That file contains some text.
+
+You should create a program that reads the essay.txt file,
+converts the first letter of each word to uppercase and prints out the converted text.
+'''
+
 
