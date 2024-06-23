ab4a0820ec693122508bb93407a604977d0b2d3d
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-22 23:16:29 -1200

    Update L60example.py

--- sec7/L60example.py
+++ sec7/L60example.py
@@ -1,3 +1,10 @@
 file = open("bear.txt")
 content = file.read()
 print(content)
+
+'''
+task 
+On the side panel there's a bear.txt file. The existing code opens that file in read mode.
+
+Below that code, please read the file content and print out the content.
+'''
