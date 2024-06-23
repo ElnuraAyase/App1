eddd2aa2f8a935473341a4ea19729d9d1300bebe
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-22 23:47:02 -1200

    Update L64Exr.py

--- sec7/L64Exr.py
+++ sec7/L64Exr.py
@@ -4,5 +4,7 @@ Take a look at the essay.txt file tab. That file contains some text.
 You should create a program that reads the essay.txt file,
 converts the first letter of each word to uppercase and prints out the converted text.
 '''
-
+a = open("essay.txt", 'r')
+b = a.read()
+print(b.title())
 
