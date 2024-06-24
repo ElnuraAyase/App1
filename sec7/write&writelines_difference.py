59bd91f89a33cbb4dde8f92a3556d0ca9d306cdb
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-24 03:02:15 -1200

    Create write&writelines_difference.py

---
+++ sec7/write&writelines_difference.py
@@ -0,0 +1,5 @@
+file = open('any.txt', 'w')
+file.write('hello he is the text for this code \n where you can see the difference') # for reading simple string
+
+file.writelines(['line1\n,', [line2\n]] # for reading the list
+file.close()
