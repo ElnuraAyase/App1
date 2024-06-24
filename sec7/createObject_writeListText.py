7e547a8e5102f1f1b7bfd9008c4526db6f066e8a
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-24 02:53:21 -1200

    Create createObject_writeListText.py

---
+++ sec7/createObject_writeListText.py
@@ -0,0 +1,4 @@
+file = open("list.txt.", "w")      #open function creates the file  to overwrite or change text
+file = open("list.txt.", "w")      #open function createsb to read what inside this file
+file.writelnines(['Line1\n', 'Line2\n')             # method for file objects t write , \n for new line
+file.readlines([])              #   method for file objects to read
