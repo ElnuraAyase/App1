55356cdc9baa101f1d49644c94075fc80cad3a0d
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-24 03:04:05 -1200

    Update write&writelines_difference.py

--- sec7/write&writelines_difference.py
+++ sec7/write&writelines_difference.py
@@ -3,3 +3,8 @@ file.write('hello he is the text for this code \n where you can see the differen
 
 file.writelines(['line1\n,', [line2\n]] # for reading the list
 file.close()
+
+file.readlines()
+              # output is a list with multiple string objects
+file.read()
+                # outpus is jsut a plane text
