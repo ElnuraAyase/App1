27c880aaa351bced17f98dbc24bfad41d51fbe0a
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-23 21:58:29 -1200

    Update and rename L64example.py to L64exr_ read_with.py

--- sec7/L64exr_ read_with.py
+++ sec7/L64exr_ read_with.py
@@ -1,10 +1,15 @@
 file = open("bear.txt")
 content = file.read()
 print(content)
-
+file.close()   # not forget to close, otherwise data leakacge 
 '''
 task 
 On the side panel there's a bear.txt file. The existing code opens that file in read mode.
 
 Below that code, please read the file content and print out the content.
 '''
+
+with open("bear.txt", 'r') as file:
+    content = file.read()
+    print(content)
+
