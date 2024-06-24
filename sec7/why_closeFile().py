6784bd77e5e197be3c4e6e44004019f6d3901ad3
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-24 00:07:51 -1200

    Update why_closeFile().py

--- sec7/why_closeFile().py
+++ sec7/why_closeFile().py
@@ -2,3 +2,8 @@
 some parts of the code may interract with each other and change the content of the file.
 so it is better to close thr file or use with statement:
 '''
+a = open("essay.txt", 'r')
+b = a.read()
+print(b.title())
+
+a.close()
