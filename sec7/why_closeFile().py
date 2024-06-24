b5e5f9dd81e09cd4dd845423d430ffc508abc1d9
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-24 00:08:54 -1200

    Update why_closeFile().py

--- sec7/why_closeFile().py
+++ sec7/why_closeFile().py
@@ -7,3 +7,21 @@ b = a.read()
 print(b.title())
 
 a.close()
+
+# other examples
+
+file = open("bear.txt")
+content = file.read()
+print(content)
+file.close()   # not forget to close, otherwise data leakacge 
+
+'''
+task 
+On the side panel there's a bear.txt file. The existing code opens that file in read mode.
+
+Below that code, please read the file content and print out the content.
+'''
+
+with open("bear.txt", 'r') as file:   # with statement
+    content = file.read()
+    print(content)
