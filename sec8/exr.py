15d9328bc365a3b5c3e75105660c2da4c99d3066
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-25 22:43:07 -1200

    Update exr.py

--- sec8/exr.py
+++ sec8/exr.py
@@ -1,1 +1,5 @@
+'''
+Extend the given code (in the exercise area) so the code capitalizes all the names and surnames of the list and then prints the new list. The output of your code should be as below:
 
+['John Smith', 'Jay Santi', 'Eva Kuki']
+'''
