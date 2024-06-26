6478f427fa6e055f3846b93c84bc1700f4d28d07
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-25 22:48:58 -1200

    Update exr_number_theList_elements.py

--- sec8/exr_number_theList_elements.py
+++ sec8/exr_number_theList_elements.py
@@ -1,1 +1,10 @@
+'''
+Extend the given code so the code prints out a list containing the number of characters for each username.
 
+The output of your code should be as below:
+
+[9, 11, 11]
+for 
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+
+....
