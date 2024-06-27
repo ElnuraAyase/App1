5c932e8d89c34ac50f9b505a339fec414180621c
Elnura <105635117+ElnuraAyase@users.noreply.github.com>
2024-06-26 23:03:39 -1200

    Update web.py

--- sec20/web.py
+++ sec20/web.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("My ToDo app")
+st.subheader("This is my app as my Thesis project")
+st.write("This app is developed to increase your productivity and help with time management")
+
+st.checkbox("1")
+st.checkbox("2")
